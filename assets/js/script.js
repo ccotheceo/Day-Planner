31803776d1a5246ab4364d380f0044de8346662f
Chuck Onyekwelu <ccotheceo@gmail.com>
2020-01-23 09:21:29 -0600

    changed up some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,61 @@
 $(document).ready(function () {
 
-    //First, we display current day, date, and time in the jumbotron.
-    var currentDayEl = $("#currentDay");
+    var todayEl = $("#today");
 
-    currentDayEl.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-    var inputBlockEl = $(".time-block");
+    todayEl.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    var inputEl = $(".time-block");
     var saveBtns = $(".saveBtn");
     var data = {};
 
     var update = function () {
-        currentDayEl.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+        todayEl.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-        var now = moment().hour(); //returns 0 through 23, aka military time.
-        //if inputblockEl is < now; then change class to past.            
-        //if inputblockEl == now; then change class to present.
-        //if inputblockEl > now, then change class to future !!! This is NOT needed because no past will become future for the day. Also, all blocks are set as future as default.
+    var now = moment().hour(); 
 
-        //Then, we calculate the time of day with moment, and then change the classes of the rows accordingly.
-
-        inputBlockEl.each(function (i, element) {
-            element = $(element);
-            if (now > element.attr("data-hour")) {
-                element.addClass("past").removeClass("future present");
-            } else if (now == element.attr("data-hour")) {
-                element.addClass("present").removeClass("future past");
+    inputEl.each(function (i, element) {
+        element = $(element);
+        if (now > element.attr("data-hour")) {
+            element.addClass("past").removeClass("future present");
+            } 
+        else if (now == element.attr("data-hour")) {
+            element.addClass("present").removeClass("future past");
             }
         });
     }
-    // we're just gonna check every SECOND.
+
     setInterval(update, 1000);
 
-    //When the user clicks on the save icon, it should save their string to local storage.
-
     saveBtns.on("click", function (event) {
-        //save userInput as a string to local storage. with THIS button!
+
+
         var button = $(this);
 
         var btnData = $(this).attr("data-time");
-        //console log click on THIS specific button in this class
         console.log(btnData);
-        //descriptionTarget locates the specific description element that user filled in text with.
-        var descriptionTarget = $("#description" + btnData);
-        console.log(descriptionTarget);
-        //we log to ensure we're grabbing the user input.
-        console.log(descriptionTarget.val());
 
-        //this is supposed to set the text area text to that specific textArea and store it.
-        //We pick the "id" attribute from the element, descriptionTarget. This id is the key, and the key value is descriptionTarget.val().
-        localStorage.setItem(descriptionTarget.attr("id"), descriptionTarget.val());
+
+        var dTarget = $("#d" + btnData);
+        console.log(dTarget);
+
+
+        console.log(dTarget.val());
+
+       
+        localStorage.setItem(dTarget.attr("id"), dTarget.val());
     });
 
-    //site should load any saved data from localStorage.
-    $("#description9").text(localStorage.getItem("description9"));
-    $("#description10").text(localStorage.getItem("description10"));
-    $("#description11").text(localStorage.getItem("description11"));
-    $("#description12").text(localStorage.getItem("description12"));
-    $("#description13").text(localStorage.getItem("description13"));
-    $("#description14").text(localStorage.getItem("description14"));
-    $("#description15").text(localStorage.getItem("description15"));
-    $("#description16").text(localStorage.getItem("description16"));
-    $("#description17").text(localStorage.getItem("description17"));
 
-    //clear button clears local storage.
+    $("#d1").text(localStorage.getItem("d1"));
+    $("#d2").text(localStorage.getItem("d2"));
+    $("#d3").text(localStorage.getItem("d3"));
+    $("#d4").text(localStorage.getItem("d4"));
+    $("#d5").text(localStorage.getItem("d5"));
+    $("#d6").text(localStorage.getItem("d6"));
+    $("#d7").text(localStorage.getItem("d7"));
+    $("#d8").text(localStorage.getItem("d8"));
+    $("#d9").text(localStorage.getItem("d9"));
+
+
     $("#clear").on("click", function () {
         console.log("clicked");
         localStorage.clear();
